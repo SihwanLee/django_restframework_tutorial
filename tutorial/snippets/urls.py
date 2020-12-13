18c0418fc32f0b0208814ab533c6c2a4aead0154
Sihwan Lee <alexxlee66@gmail.com>
2020-12-14 02:01:34 +0900

    Django Rest Framework - Tutorial 3: Class-based Views
    Rewrote API using class-based-views
    Used Mixins / Used generic class-based views

--- tutorial/snippets/urls.py
+++ tutorial/snippets/urls.py
@@ -3,8 +3,8 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from snippets import views
 
 urlpatterns = [
-	path('', views.snippet_list),
-	path('<str:pk>/', views.snippet_detail),
+	path('', views.SnippetList.as_view()),
+	path('<str:pk>/', views.SnippetDetail.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
